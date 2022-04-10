2ba249a0c6c19eccc2e39c528899daf95f094b99
KhilariDivya23 <97224339+KhilariDivya23@users.noreply.github.com>
2022-04-10 16:50:36 +0530

    Update CountNegativeNumbers.py

--- Day-14/CountNegativeNumbers.py
+++ Day-14/CountNegativeNumbers.py
@@ -1,8 +1,16 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] < 0:
-                    count += 1
+        m, n, count = len(grid[0]), len(grid), 0
+        for row in grid:
+            start, end = 0, m - 1
+            while start <= end:
+                mid = (start + end) // 2
+                if row[mid] < 0:
+                    if mid == 0 or row[mid-1] >= 0:
+                        count += m-mid
+                        break                        
+                    else:
+                        end = mid - 1
+                else:
+                    start = mid + 1
         return count
