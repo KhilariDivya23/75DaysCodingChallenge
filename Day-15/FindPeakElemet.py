267b7d50b4881935bc9cf942a14b826cf85e33dc
KhilariDivya23 <97224339+KhilariDivya23@users.noreply.github.com>
2022-04-10 21:18:11 +0530

    Update FindPeakElemet.py

--- Day-15/FindPeakElemet.py
+++ Day-15/FindPeakElemet.py
@@ -1,4 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        nums_dict = {j:i for i, j in enumerate(nums)}
-        return nums_dict[max(nums)]
+        start, end, n = 0, len(nums)- 1, len(nums)
+        while start <= end:
+            mid = (start + end) // 2
+            if mid == 0 and end == 1:
+                if nums[mid] > nums[end]:
+                    return mid
+                return end
+            elif mid == 0 or mid == n - 1:
+                return mid
+            elif nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+                return mid
+            elif nums[mid] > nums[mid -1]:
+                start = mid + 1
+            else:
+                end = mid - 1    
