c02fccba2d456b78000b2b4140ef702e9359632c
KhilariDivya23 <97224339+KhilariDivya23@users.noreply.github.com>
2022-04-10 23:00:26 +0530

    Update pow(x,n).py

--- Day-15/pow(x,n).py
+++ Day-15/pow(x,n).py
@@ -1,3 +1,12 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        return float(x ** n)
+    def myPow(self, x: float, no: int) -> float:
+        ans, n = 1, abs(no)
+        while n != 0:
+            if n % 2 == 0:
+                x, n = x*x, n / 2
+            else:
+                ans *= x
+                n -= 1
+        if no < 0:
+            ans = 1 / ans
+        return ans
